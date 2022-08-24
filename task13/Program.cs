8746dfb48d461e8da9e06874107d0a53c3c6ac16
Anton Rybak <toxa-wow@mail.ru>
2022-08-24 03:00:55 +0300

    task13 done

---
+++ task13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+string s = Convert.ToString(number);
+if (s.Length <= 2){
+    Console.WriteLine("Третьей цифры нет");
+}
+else Console.WriteLine(s[2]);
